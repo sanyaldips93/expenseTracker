39414e9604a3afdebcbf08ee3d20e2ceaf646e1b
Dipayan Sanyal <dipayan.s@goavega.com>
2021-03-10 14:31:41 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center' }}>Expense Tracker</h1>
+      <br />
+    </div>
+  )
+}
+
+export default Header;
